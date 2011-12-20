abc853c479654f63685db57adfcbe40ec6ce1305
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-12-20 15:07:29 +0900

    updated gemspec

--- lib/enju_resource_merge/engine.rb
+++ lib/enju_resource_merge/engine.rb
@@ -1,3 +1,11 @@
+require 'devise'
+require 'cancan'
+require 'inherited_resources'
+require 'attribute_normalizer'
+require 'will_paginate'
+require 'sunspot_rails'
+require 'dynamic_form'
+
 module EnjuResourceMerge
   class Engine < Rails::Engine
   end
