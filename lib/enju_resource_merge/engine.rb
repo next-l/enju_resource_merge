8bebf32ff27cb58affdea8fbe15bd34e45ff8dcf
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-06-19 20:02:12 +0900

    require enju_core

--- lib/enju_resource_merge/engine.rb
+++ lib/enju_resource_merge/engine.rb
@@ -1,11 +1,6 @@
-require 'devise'
-require 'cancan'
+require 'enju_core'
 require 'inherited_resources'
-require 'attribute_normalizer'
-require 'will_paginate'
-require 'sunspot_rails'
 require 'dynamic_form'
-require 'acts_as_list'
 
 module EnjuResourceMerge
   class Engine < ::Rails::Engine
